0660578435d2622c0583eb443449948a8295ab8a
leaderahad <leaderahad@gmail.com>
2025-05-17 21:33:05 +0600

    Update rBasics(Part-2).R

---
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/rBasics(Part-2).R
@@ -0,0 +1,26 @@
+# Data types in R
+# 1. Numeric - Integer / Floating point
+# 2. String/Character - "Ahad" / 'Ahad' 
+# 3. Logical 
+
+a = 11
+b = 7.19
+class(a)
+class(b)
+
+name = "Ahad"
+print(name)
+class(name)
+
+c = "52"
+print(c + 9)
+print(a+9)
+class(c)
+
+# logical data type - TRUE, FALSE (T, F)
+print(a < b)
+x = 19
+y = 22-3
+print(x == y)
+print( x!= y)
+print(y >= x)
